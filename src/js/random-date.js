4661701296dc8b012afc620e05bd17f509f4d86e
Joseph Friesen <friesen.josephc@gmail.com>
2018-10-05 09:45:22 -0700

    Add random-date.js for testing purposes, to create a person object with a random birth date.

---
+++ src/js/random-date.js
@@ -0,0 +1,30 @@
+/*
+
+import { RandomDate } from './../src/js/random-date.js';
+*/
+
+function export rand(lowerBound, upperBound) {
+  return Math.floor(Math.random() * (upperBound - lowerBound + 1)) + lowerBound;
+};
+function export RandomDate() {
+  this.randYear = rand(1940,2015);
+  this.randMonth = rand(0,11);
+  this.randDay = this.getDay();
+  this.date = new Date(this.randYear, this.randMonth, this.randDay);
+};
+RandomDate.prototype.getDay = function() {
+  if ([3,5,8,10].includes(this.randMonth)) {
+    return rand(1,30);
+  }
+  else if (this.randMonth == 1) {
+    if (this.randYear % 4 == 0) {
+      return rand(1,29);
+    }
+    else {
+      return rand(1,28);
+    }
+  }
+  else {
+    return rand(1,31);
+  }
+};
