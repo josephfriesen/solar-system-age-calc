583825074faf676728615e496a01f02914561a8f
Joseph Friesen <friesen.josephc@gmail.com>
2018-10-05 16:29:11 -0700

    Add functionality to display age and life expectancy after choosing user birthdate and planet.

--- src/main.js
+++ src/main.js
@@ -23,9 +23,63 @@ $(document).ready(function() {
   let User;
   $("#birthdate-button").click(function() {
     const birthdate = $("#birthdate").val();
-    const d = new Date(birthdate);
-    User = new Person(d);
-    $(".planets").slideDown();
+    if (birthdate !== "") {
+      const d = new Date(birthdate);
+      User = new Person(d);
+      $(".planets").toggleClass("hidden");
+      $(".planets").toggleClass("flex");
+    }
+  });
+  $("#mercury").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Mercury");
+    $(".planet-age").text(User.mercuryAge().toFixed(2));
+    $(".expectancy").text(User.mercuryLifeLeft().toFixed(2));
+  });
+  $("#venus").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Venus");
+    $(".planet-age").text(User.venusAge().toFixed(2));
+    $(".expectancy").text(User.venusLifeLeft().toFixed(2));
+  });
+  $("#earth").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Earth");
+    $(".planet-age").text(User.earthAge().toFixed(2));
+    $(".expectancy").text(User.earthLifeLeft().toFixed(2));
+  });
+  $("#mars").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Mars");
+    $(".planet-age").text(User.marsAge().toFixed(2));
+    $(".expectancy").text(User.marsLifeLeft().toFixed(2));
+  });
+  $("#jupiter").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Jupiter");
+    $(".planet-age").text(User.jupiterAge().toFixed(2));
+    $(".expectancy").text(User.jupiterLifeLeft().toFixed(2));
+  });
+  $("#saturn").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Saturn");
+    $(".planet-age").text(User.saturnAge().toFixed(2));
+    $(".expectancy").text(User.saturnLifeLeft().toFixed(2));
+  });
+  $("#neptune").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Neptune");
+    $(".planet-age").text(User.neptuneAge().toFixed(2));
+    $(".expectancy").text(User.neptuneLifeLeft().toFixed(2));
+  });
+  $("#uranus").click(function() {
+    $(".overlay").show();
+    $(".planet-label").text("Uranus");
+    $(".planet-age").text(User.uranusAge().toFixed(2));
+    $(".expectancy").text(User.uranusLifeLeft().toFixed(2));
   });
 
+  $("#close-overlay").click(function() {
+    $(".overlay").hide();
+  });
 });
