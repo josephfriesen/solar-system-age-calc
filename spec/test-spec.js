2c6b39f7a5e5992431b3d735ceaf5104d699199e
Joseph Friesen <friesen.josephc@gmail.com>
2018-10-05 09:35:55 -0700

    Add outline of what the Person object should look like, preliminary tests that all fail.

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,7 +1,54 @@
+import { Person } from './../src/js/person.js';
 
+describe('The "Person" object and its associated prototypes', function() {
 
-describe('Testing my shit.', function() {
-  it('should test whether 1 is equal to 1.', function() {
-    expect(1).toEqual(1);
+  const today = new Date();
+  const SomeDude = new Person(today);
+
+  beforeEach(function() {
+    //
   });
+
+  // comment this first test out after, just checking that I understand how Date objects work.
+  it('should give the test variable "today" as the current date', function() {
+
+    expect(today.getDay()).toEqual(5); // is today Friday?
+    expect(today.getDate()).toEqual(5); // is today the fifth?
+    expect(today.getMonth()).toEqual(9); // is it October?
+    expect(today.getFullYear()).toEqual(2018); // is it 2018?
+
+  });
+
+  it('should show that, given a specific birthdate, the Person object has correctly recorded that date as a value of the key "Person.birthdate"', function() {
+
+    expect(SomeDude.birthdate).toEqual(null);
+
+  });
+
+  it('should show that the birth year, month and day have all been read from the birthdate object and been recorded under the appropriate key', function() {
+
+    expect(SomeDude.year).toEqual(null);
+    expect(SomeDude.month).toEqual(null);
+    expect(SomeDude.day).toEqual(null);
+
+  });
+
+  it('should show that the person object has correctly calculated their (earth) age', function() {
+
+    expect(SomeDude.age).toEqual(null);
+
+  });
+
+  it('should test that the method for each planet is scaling the age of that person correctly', function() {
+
+    expect(SomeDude.mercuryAge()).toEqual(null);
+    expect(SomeDude.venusAge()).toEqual(null);
+    expect(SomeDude.marsAge()).toEqual(null);
+    expect(SomeDude.jupiterAge()).toEqual(null);
+    expect(SomeDude.saturnAge()).toEqual(null);
+    expect(SomeDude.neptuneAge()).toEqual(null);
+    expect(SomeDude.uranusAge()).toEqual(null);
+
+  });
+
 });
---
+++ src/js/person.js
@@ -0,0 +1,43 @@
+/*
+
+The Person object will instantiate with the user's birthdate and will have a collection of methods that will be used to calculate the user's age on other planets, e.g. suppose I create a user Joe (me!), a method (to be defined later) Joe.marsAge() will return Joe.earthAge()/1.88.
+
+*/
+
+export function Person(birthdate) {
+  this.birthdate = birthdate;
+  this.age = this.earthAge();
+  this.today = new Date();
+};
+
+Person.prototype.earthAge = function() {
+  return 0;
+};
+
+Person.prototype.mercuryAge = function() {
+  return 0;
+};
+
+Person.prototype.venusAge = function() {
+  return 0;
+};
+
+Person.prototype.marsAge = function() {
+  return 0;
+};
+
+Person.prototype.jupiterAge = function() {
+  return 0;
+};
+
+Person.prototype.saturnAge = function() {
+  return 0;
+};
+
+Person.prototype.neptuneAge = function() {
+  return 0;
+};
+
+Person.prototype.uranusAge = function() {
+  return 0;
+};
